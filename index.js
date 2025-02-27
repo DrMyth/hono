ce6a3b690605c1365688130506d6982e779a4ce3
varun <varunmaramreddy1@gmail.com>
2025-02-27 17:01:11 +0530

    Init

---
+++ index.js
@@ -0,0 +1,8 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => c.text('Hello, Cloudflare Workers!'))
+app.get('/api', (c) => c.json({ message: 'Hono is awesome!' }))
+
+export default app
