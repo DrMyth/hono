8c51d0689fdceb1108219b3944c40c061bc89112
Varun Maramreddy <128990962+DrMyth@users.noreply.github.com>
2025-02-28 12:19:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 
 const app = new Hono();
 
-app.get('/', (c) => c.text('Hello, Cloudflare Workers!'))
+app.get('/', (c) => c.text('Hello, Cloudflare Workers!'));
 app.get('/api', (c) => c.json({ message: 'Hono is awesome!' }))
 
 export default app
