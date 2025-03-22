ba87f0eaf62e1e2de872ffb85789229e9812d8ab
varun <varunmaramreddy1@gmail.com>
2025-03-22 17:22:55 +0530

    polly the final commit �

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text(`Hello, Cloudflare Workers! hi ${process.env.VAR1}`));
-app.get('/api', (c) => c.json({ message: `Hono is awesome! hello there *========* ${process.env.VAR2}`}))
-app.get('/gt', (c) => c.json({ message: `BBALLLASFA HFAODFSDgf sda ${process.env.VAR3}`}))
+app.get('/', (c) => c.text(`Hello, Cloudflare Workers! hi ${c.env.VAR1}`))
+app.get('/api', (c) => c.json({ message: `Hono is awesome! hello there *========* ${c.env.VAR2}` }))
+app.get('/gt', (c) => c.json({ message: `BBALLLASFA HFAODFSDgf sda ${c.env.VAR3}` }))
 
 export default app
