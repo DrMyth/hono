348a838eeb9fb4ca3a966438aeabfc6584954178
varun <varunmaramreddy1@gmail.com>
2025-03-22 15:27:27 +0530

    updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 const app = new Hono()
 
 app.get('/', (c) => c.text(`Hello, Cloudflare Workers! hi ${process.env.VAR1}`));
-app.get('/api', (c) => c.json({ message: 'Hono is awesome! hello there *========*' }))
-app.get('/gt', (c) => c.json({ message: 'BBALLLASFA HFAODFSDgf sda' }))
+app.get('/api', (c) => c.json({ message: `Hono is awesome! hello there *========* ${process.env.VAR2}`}))
+app.get('/gt', (c) => c.json({ message: `BBALLLASFA HFAODFSDgf sda ${process.env.VAR3}`}))
 
 export default app
