688192b03cb56ac3f88f26c7fbfd6a16e7dbefb3
Varun Maramreddy <128990962+DrMyth@users.noreply.github.com>
2025-04-01 17:48:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = new Hono()
 
 app.get('/', (c) => c.text(`Hello, Cloudflare Workers! hi ${c.env.VAR1}`))
 app.get('/api', (c) => c.json({ message: `Hono is awesome! hello there *========* ${c.env.VAR2}` }))
-app.get('/gt', (c) => c.json({ message: `BBALLLASFA HFAODFSDgf sda ${c.env.VAR3}` }))
+app.get('/gt', (c) => c.json({ message: `BBALLLASFA HFAODFSDgf sda ${c.env.VAR3} ${process.env}` }))
 
 export default app
