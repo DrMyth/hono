1418baecdf2157a1cabea54a829c8f6b2e74a89f
Varun Maramreddy <128990962+DrMyth@users.noreply.github.com>
2025-04-11 23:38:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text(`Hello, Cloudflare Workers! hi ${c.env.VAR1}`))
-app.get('/api', (c) => c.json({ message: `Hono is awesome! hello there *========* ${c.env.VAR2}` }))
-app.get('/gt', (c) => c.json({ message: `BBALLLASFA HFAODFSDgf sda ${c.env.VAR3}` }))
+app.get('/', (c) => c.text(`Hello, Cloudflare Workers! hi ${c.env.VAR1} Updated V1`))
+app.get('/api', (c) => c.json({ message: `Hono is awesome! hello there *========* ${c.env.VAR2} Updated V1` }))
+app.get('/gt', (c) => c.json({ message: `BBALLLASFA HFAODFSDgf sda ${c.env.VAR3} Updated V1` }))
 app.get('/env', (c) => {
   return c.json({ env: c.env })
 })
